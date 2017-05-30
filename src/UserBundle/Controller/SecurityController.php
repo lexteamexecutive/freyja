1e35e3357f2439a2dbdeab6132e5683a9548117c
KeurbyCandy <cavannachristophe@gmail.com>
2017-05-30 16:41:48 +0200

    fixup! add template

--- src/UserBundle/Controller/SecurityController.php
+++ src/UserBundle/Controller/SecurityController.php
@@ -5,7 +5,6 @@ namespace UserBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Validator\ConstraintViolation;
 use UserBundle\Form\UserType;
 use UserBundle\Entity\User;
 
