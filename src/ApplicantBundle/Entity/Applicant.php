172bcc67f1b8e9143d83985258228b0aa0079d0b
KeurbyCandy <cavannachristophe@gmail.com>
2017-07-06 11:10:13 +0200

    feat(applicant)

---
+++ src/ApplicantBundle/Entity/Applicant.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ApplicantBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+
+/**
+ * @ORM\Table(name="applicants")
+ * @ORM\Entity(repositoryClass="ApplicantBundle\Repository\ApplicantRepository")
+ * @UniqueEntity(fields="username", message="Cet Email est déjà utilisé")
+ */
+class Applicant
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $lastName;
+}
---
+++ src/ApplicantBundle/Repository/ApplicantRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ApplicantBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class ApplicantRepository extends EntityRepository
+{
+}
